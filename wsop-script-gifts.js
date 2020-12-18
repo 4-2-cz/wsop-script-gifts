a6a83863cd9df0cb0e067b3a7f7c7520f237db93
pida42 <10919081+pida42@users.noreply.github.com>
2020-12-18 04:13:15 +0100

    Update wsop-script-gifts.js
    latest

--- wsop-script-gifts.js
+++ wsop-script-gifts.js
@@ -32,80 +32,81 @@
 // ==/UserScript==
 
 (function (jQuery, open) {
-    'use strict';
+  'use strict';
 
-    console.log('WSOP Script Gifts :: Initialize');
+  console.log('WSOP Script Gifts :: Initialize');
 
-    var waitForElement = {
-        _i: null,
-        _c: 0,
-        _m: 60,
-        wait: function(elementSelector, callback) {
-            if(null !== waitForElement._i) return false;
-            callback = callback || function(){};
-            clearInterval(waitForElement._i);
-            waitForElement._i = null;
-            waitForElement._i = window.setInterval(function() {
-                waitForElement._c = waitForElement._c + 1;
-                var waitElement = jQuery(elementSelector);
-                if(waitElement.length) {
-                    clearInterval(waitForElement._i);
-                    waitForElement._i = null;
-                    callback(waitElement);
-                }
-                if(waitForElement._c >= waitForElement._m) {
-                    clearInterval(waitForElement._i);
-                    waitForElement._i = null;
-                    return false;
-                }
-            }, 1000);
+  var waitForElement = {
+    _i: null,
+    _c: 0,
+    _m: 60,
+    wait: function (elementSelector, callback) {
+      if (null !== waitForElement._i) return false;
+      callback = callback || function () {};
+      clearInterval(waitForElement._i);
+      waitForElement._i = null;
+      waitForElement._i = window.setInterval(function () {
+        waitForElement._c = waitForElement._c + 1;
+        var waitElement = jQuery(elementSelector);
+        if (waitElement.length) {
+          clearInterval(waitForElement._i);
+          waitForElement._i = null;
+          callback(waitElement);
         }
-    };
-
-    var inboxButtonSelector = '#lobby > div.lobbyBottom > div.lobbyBottomRightButtons > button.btn.btn-lobbySide.inbox.notification';
-    var inboxSectionTitleSelector = '#inbox > div.scrollableArea > div:nth-child(1) > div > div.sections > div.inboxSection.gifts.hasNewItems.Master > div';
-    var collectAllButtonSelector = '#inbox > div.scrollableArea.hasVerticallyScroll > div:nth-child(1) > div > div.sections > div.inboxSection.gifts.opened.hasNewItems.Master > div.titleContainer > button.btn.genericButton.blue.collectAll';
-
-    waitForElement.wait(inboxButtonSelector, function(inboxButton) {
-
-        console.log('WSOP Script Gifts :: waitForElement.callback -> inboxButton');
-
-        if(typeof inboxButton[0] !== 'undefined' && jQuery(inboxButton[0]).length) {
-
-            jQuery(inboxButton[0]).click();
-            console.log('WSOP Script Gifts :: waitForElement.callback -> inboxButton -> click trigger done');
-
-            waitForElement.wait(inboxSectionTitleSelector, function(inboxSectionTitle) {
-
-                console.log('WSOP Script Gifts :: waitForElement.callback -> inboxSectionTitle');
-
-                if(typeof inboxSectionTitle[0] !== 'undefined' && jQuery(inboxSectionTitle[0]).length) {
-
-                    jQuery(inboxSectionTitle[0]).click();
-                    console.log('WSOP Script Gifts :: waitForElement.callback -> inboxSectionTitle -> click trigger done');
-
-                    waitForElement.wait(collectAllButtonSelector, function(collectAllButton) {
-
-                        console.log('WSOP Script Gifts :: waitForElement.callback -> collectAllButton');
-
-                        if(typeof collectAllButton[0] !== 'undefined' && jQuery(collectAllButton[0]).length) {
-
-                            var giftsCount = jQuery(inboxButtonSelector).data('count');
-                            console.log('WSOP Script Gifts :: waitForElement.callback -> GIFTS: ' + giftsCount);
-
-                            if(parseInt(giftsCount)) {
-                                jQuery(collectAllButton[0]).click();
-                                console.log('WSOP Script Gifts :: waitForElement.callback -> collectAllButton -> CHIPS COLLECTED');
-                            } else {
-                                console.log('WSOP Script Gifts :: waitForElement.callback -> collectAllButton -> there is no gift waiting');
-                            }
-                        }
-
-                    });
-                }
-
-            });
+        if (waitForElement._c >= waitForElement._m) {
+          clearInterval(waitForElement._i);
+          waitForElement._i = null;
+          return false;
         }
-    });
+      }, 1000);
+    }
+  };
+
+  var inboxButtonSelector = '#lobby > div.lobbyBottom > div.lobbyBottomRightButtons > button.btn.btn-lobbySide.inbox.notification';
+  var inboxSectionTitleSelector = '#inbox > div.scrollableArea > div:nth-child(1) > div > div.sections > div.inboxSection.gifts.hasNewItems.Master > div';
+  var collectAllButtonSelector = '#inbox > div.scrollableArea.hasVerticallyScroll > div:nth-child(1) > div > div.sections > div.inboxSection.gifts.opened.hasNewItems.Master > div.titleContainer > button.btn.genericButton.blue.collectAll';
+
+  waitForElement.wait(inboxButtonSelector, function (inboxButton) {
+
+    console.log('WSOP Script Gifts :: waitForElement.callback -> inboxButton');
+
+    if (typeof inboxButton[0] !== 'undefined' && jQuery(inboxButton[0]).length) {
+
+      jQuery(inboxButton[0]).click();
+      console.log('WSOP Script Gifts :: waitForElement.callback -> inboxButton -> click trigger done');
+
+      waitForElement.wait(inboxSectionTitleSelector, function (inboxSectionTitle) {
+
+        console.log('WSOP Script Gifts :: waitForElement.callback -> inboxSectionTitle');
+
+        if (typeof inboxSectionTitle[0] !== 'undefined' && jQuery(inboxSectionTitle[0]).length) {
+
+          jQuery(inboxSectionTitle[0]).click();
+          console.log('WSOP Script Gifts :: waitForElement.callback -> inboxSectionTitle -> click trigger done');
+
+          waitForElement.wait(collectAllButtonSelector, function (collectAllButton) {
+
+            console.log('WSOP Script Gifts :: waitForElement.callback -> collectAllButton');
+
+            if (typeof collectAllButton[0] !== 'undefined' && jQuery(collectAllButton[0]).length) {
+
+              var giftsCount = jQuery(inboxButtonSelector).data('count');
+              console.log('WSOP Script Gifts :: waitForElement.callback -> GIFTS: ' + giftsCount);
+
+              if (parseInt(giftsCount)) {
+                jQuery(collectAllButton[0]).click();
+                console.log('WSOP Script Gifts :: waitForElement.callback -> collectAllButton -> CHIPS COLLECTED');
+              }
+              else {
+                console.log('WSOP Script Gifts :: waitForElement.callback -> collectAllButton -> there is no gift waiting');
+              }
+            }
+
+          });
+        }
+
+      });
+    }
+  });
 
 })(jQuery);
